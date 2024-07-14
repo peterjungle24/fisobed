1e437ff62be8bc922c24f1a4e782ebf1635f0954
Slugg (MARSHAWW) <153966494+peterjungle24@users.noreply.github.com>
2024-07-14 16:25:42 -0300

    glow_sait.cs but + or - better

--- Creatures/Glowing Sait/glow_sait.cs
+++ Creatures/Glowing Sait/glow_sait.cs
@@ -9,15 +9,12 @@ using circle;
 
 namespace sait
 {
-
     #region TT = Template.Type
 
     //SAIT template type
     public static class TT_sait
     {
-
-        public static readonly CreatureTemplate.Type TT_glow_sait = new CreatureTemplate.Type("glow_sait", true);
-
+        public static readonly CreatureTemplate.Type TT_glow_sait = new CreatureTemplate.Type("glow_sait", true);    //register the fucking creature into the static readonly field
     }
 
     #endregion
@@ -26,22 +23,19 @@ namespace sait
     //SAIT template
     public class T_sait : CreatureTemplate
     {
-
         public T_sait(CreatureTemplate.Type sait_type, CreatureTemplate ancestor, List<TileTypeResistance> tileResistances, List<TileConnectionResistance> connectionResistances, CreatureTemplate.Relationship defaultRelationship) : base(sait_type, ancestor, tileResistances, connectionResistances, defaultRelationship)
         {
 
-            this.name = "Glowing Sait";
-            this.AI = true;
-            this.canFly = false;
-            this.canSwim = false;
-            this.grasps = 0;
+            this.name = "Glowing Sait";    //the creature name -/- string
+            this.AI = true;    //if it have AI -/- boolean
+            this.canFly = false;    //if it can fly -/- boolean
+            this.canSwim = false;    //if it can swim /-/ boolean
+            this.grasps = 0;    //how many grasps it have -/- int
 
-            base.shortcutColor = Color.green;
-            base.smallCreature = true;
-            base.type = type;
-
+            base.shortcutColor = Color.green;    //a default shortcut color /-/ UnityEngine.Color
+            base.smallCreature = true;    //if is smol creature -/- boolean
+            base.type = type;    //base
         }
-
     }
 
     #endregion
@@ -50,23 +44,21 @@ namespace sait
     //SAIT creature
     public class C_glow_sait : TubeWorm
     {
-
+        /// mpty
         public C_glow_sait(AbstractCreature abstractCreature, World world) : base(abstractCreature, world)
         {
         }
 
+        /// <summary>
+        /// override the InitiateGraphicsModule for add the fucking graphics for the lil sait
+        /// </summary>
         public override void InitiateGraphicsModule()
         {
-
-            if (base.graphicsModule == null)
+            if (base.graphicsModule == null)    //if this graphics module field is null
             {
-
-                base.graphicsModule = new TubeWormGraphics(this);
-
+                base.graphicsModule = new TubeWormGraphics(this);    //ads the graphics
             }
-
         }
-
     }
 
     #endregion
@@ -75,91 +67,59 @@ namespace sait
     {
 
         /// <summary>
-        /// apply the palette to the Sait :3
+        /// [HOOK] apply the palette to the Sait :3
         /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="sLeaser"></param>
-        /// <param name="rCam"></param>
-        /// <param name="palette"></param>
         public static void tubeworm_applyP(On.TubeWormGraphics.orig_ApplyPalette orig, TubeWormGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
         {
-
             orig(self, sLeaser, rCam, palette);                                         //call the orig
 
-            sLeaser.sprites[0].color = new Color(0.6666667f, 0.94509804f, 0.3372549f);  //change color?
-            sLeaser.sprites[sLeaser.sprites.Length - 1].color = Color.black;            //i think it changess the color here too
-
+            sLeaser.sprites[0].color = new Color(0.6666667f, 0.94509804f, 0.3372549f);  //change color
+            sLeaser.sprites[sLeaser.sprites.Length - 1].color = Color.black;            //changes the color of the rest of the index escept the first one
         }
 
         /// <summary>
-        /// ctor.
+        /// [HOOK] ctor.
         /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="ow"></param>
         public static void tubeworm_ctor(On.TubeWormGraphics.orig_ctor orig, TubeWormGraphics self, PhysicalObject ow)
         {
-
-            orig.Invoke(self, ow);
-
+            orig.Invoke(self, ow);    //it just calls the orig LMAO
         }
 
         /// <summary>
-        /// initiate the sprites to The Sait
+        /// [HOOK] initiate the sprites to The Sait
         /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="sLeaser"></param>
-        /// <param name="rCam"></param>
         public static void tubeworm_initS(On.TubeWormGraphics.orig_InitiateSprites orig, TubeWormGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
         {
-
             orig.Invoke(self, sLeaser, rCam);
-            Array.Resize<FSprite>(ref sLeaser.sprites, sLeaser.sprites.Length + 1);
-            sLeaser.sprites[sLeaser.sprites.Length - 1] = new FSprite("FaceB0", true);
-            sLeaser.sprites[sLeaser.sprites.Length - 1].color = new Color(160, 2, 2);
-            sLeaser.sprites[sLeaser.sprites.Length - 1].scale = 1.3f;
-            sLeaser.sprites[0].shader = FShader.defaultShader;
-            rCam.ReturnFContainer("Midground").AddChild(sLeaser.sprites[sLeaser.sprites.Length - 1]);
 
+            //things I don't know vs things I know:
+            Array.Resize<FSprite>(ref sLeaser.sprites, sLeaser.sprites.Length + 1);    //????
+            sLeaser.sprites[sLeaser.sprites.Length - 1] = new FSprite("FaceB0", true);    //????
+            sLeaser.sprites[sLeaser.sprites.Length - 1].color = new Color(160, 2, 2);    //????
+            sLeaser.sprites[sLeaser.sprites.Length - 1].scale = 1.3f;    //????
+            sLeaser.sprites[0].shader = FShader.defaultShader;    //????
+            rCam.ReturnFContainer("Midground").AddChild(sLeaser.sprites[sLeaser.sprites.Length - 1]);    //return and add to the container
         }
 
         /// <summary>
-        /// draw the sprites. Its a updatable
+        /// [HOOK] draw the sprites. Its a updatable
         /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="sLeaser"></param>
-        /// <param name="rCam"></param>
-        /// <param name="timeStacker"></param>
-        /// <param name="camPos"></param>
         public static void tubeworm_drawS(On.TubeWormGraphics.orig_DrawSprites orig, TubeWormGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
+            orig.Invoke(self, sLeaser, rCam, timeStacker, camPos);    //calls the orig
 
-            orig.Invoke(self, sLeaser, rCam, timeStacker, camPos);
-
-            sLeaser.sprites[0].alpha = 1f;
+            sLeaser.sprites[0].alpha = 1f;    //alpha of the sprite
+            //i dont will explain the rest
             sLeaser.sprites[sLeaser.sprites.Length - 1].SetPosition((Vector2.Lerp(self.bodyParts[2].lastPos, self.bodyParts[2].pos, timeStacker) + Vector2.Lerp(self.bodyParts[1].lastPos, self.bodyParts[1].pos, timeStacker)) / 2f - camPos);
             sLeaser.sprites[sLeaser.sprites.Length - 1].MoveToFront();
-
         }
 
         /// <summary>
-        /// add the thing to container.
+        /// [HOOK] add the thing to container.
         /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="sLeaser"></param>
-        /// <param name="rCam"></param>
-        /// <param name="newContatiner"></param>
         public static void tubeworm_container(On.TubeWormGraphics.orig_AddToContainer orig, TubeWormGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
         {
-
-            orig.Invoke(self, sLeaser, rCam, newContatiner);
-
+            orig.Invoke(self, sLeaser, rCam, newContatiner);    //call the orig....
         }
-
     }
-
 }
